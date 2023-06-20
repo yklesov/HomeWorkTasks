1d0ccbe0f2917bfaa1f970e4915926c56db3a838
Юрий Клесов <klyosovyura@gmail.com>
2023-06-21 01:10:53 +0400

    Homework 20.06.23

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,52 +8,56 @@ namespace ConsoleApp2
 {
     internal class Program
     {
-        static long GetGCD(long a, long b)
-        {
-            if (a == 0)
-            {
-                return b;
-            }
+        //static long GetGCD(long a, long b)
+        //{
+        //    if (a == 0)
+        //    {
+        //        return b;
+        //    }
 
-            if (b == 0)
-            {
-                return a;
-            }
+        //    if (b == 0)
+        //    {
+        //        return a;
+        //    }
 
-            return a > b ? GetGCD(a % b, b) : GetGCD(a, b % a);
-        }
+        //    return a > b ? GetGCD(a % b, b) : GetGCD(a, b % a);
+        //}
 
-        static int Fibonachi(int n)
-        {
-            if (n == 0 || n == 1) return n;
+        //static int Fibonachi(int n)
+        //{
+        //    if (n == 0 || n == 1) return n;
 
-            return Fibonachi(n - 1) + Fibonachi(n - 2);
-        }
-        static int Factorial(int n)
-        {
-            if (n == 1) return 1;
+        //    return Fibonachi(n - 1) + Fibonachi(n - 2);
+        //}
+        //static int Factorial(int n)
+        //{
+        //    if (n == 1) return 1;
 
-            return n * Factorial(n - 1);
-        }
+        //    return n * Factorial(n - 1);
+        //}
 
 
         static void Main(string[] args)
         {
-            int fac = Factorial(10);
-            Console.WriteLine(fac);
+            //int fac = Factorial(10);
+            //Console.WriteLine(fac);
 
-            int fib = Fibonachi(16);
-            Console.WriteLine(fib);
+            //int fib = Fibonachi(16);
+            //Console.WriteLine(fib);
 
-            Console.WriteLine("Введите a и b:");
-            Console.Write(" a = ");
-            var a = Convert.ToInt64(Console.ReadLine());
-            Console.Write(" b = ");
-            var b = Convert.ToInt64(Console.ReadLine());
+            //Console.WriteLine("Введите a и b:");
+            //Console.Write(" a = ");
+            //var a = Convert.ToInt64(Console.ReadLine());
+            //Console.Write(" b = ");
+            //var b = Convert.ToInt64(Console.ReadLine());
 
-            var gcd = GetGCD(a, b);
+            //var gcd = GetGCD(a, b);
 
-            Console.WriteLine($"\nНОД равен {gcd}");
+            //Console.WriteLine($"\nНОД равен {gcd}");
+            var wood = new WoodWorkingCompany();
+            wood.Backgammon();
+            wood.Chess();
+            wood.Shogi();
 
         }
     }
---
+++ ConsoleApp2/WoodWorkingCompany.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp2
+{
+    internal class WoodWorkingCompany
+    {
+        public static string woodName = "Ring-shaped wood";
+        public int age { get; set; }
+        public float size { get; set; }
+        public void Backgammon()
+        {
+            DateTime thisDay = DateTime.Today;
+            string text = thisDay.ToString("d");
+            string[] words = text.Split(new char[] { '.' });
+            int count = int.Parse(words[0]);
+            Console.WriteLine("Hi, today we can made " + count + " backgammon");
+        }
+        public void Chess()
+        {
+            DateTime thisDay = DateTime.Today;
+            string text = thisDay.ToString("d");
+            string[] words = text.Split(new char[] { '.' });
+            int count = int.Parse(words[0]);
+            Console.WriteLine("Hi, today we can made " + count + " chess");
+        }
+        public void Shogi()
+        {
+            DateTime thisDay = DateTime.Today;
+            string text = thisDay.ToString("d");
+            string[] words = text.Split(new char[] { '.' });
+            int count = int.Parse(words[0]);
+            Console.WriteLine("Hi, today we can made " + count + " shodi");
+        }
+    }
+}
